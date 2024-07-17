4a98de8697dff73a3d4ebf8b5d04df4919e86c35
Andrew Hughes <andrewcarterhughes@gmail.com>
2024-07-17 23:05:40 +0530

    updates

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
+import anki.collection
 from anki.collection import Collection
-import csv
+from pprint import pprint
+import json
 
 col = Collection("/home/andrewcarterhughes/.local/share/Anki2/Andrew/collection.anki2")
-notes = col.find_notes("deck:LRZTP::17")
+noteIds = col.find_notes("deck:LRZTP")
 
-def clean(str):
-    return str.replace('&nbsp;', '').strip()
+def getNotetypeNameFromId(modelId):
+    model = col.models.get(modelId)
+    return model['name']
 
-with open('out.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for note in notes:
-        note_obj = col.get_note(note)
-        print(note_obj['Front'])
-        writer.writerow([clean(note_obj['Front']), clean(note_obj['Back'])])
+def getNotetypeNameFromNote(note):
+    model = col.models.get(note.mid)
+    return model['name']
+
+#note = col.get_note(notes[0])
+#notetypeName = getNotetypeNameFromNote(note)
+
+##pprint(vars(note))
+
+def getDecksForNote(note):
+    if isinstance(note, int):
+        note = col.get_note(note)
+
+    if isinstance(note, anki.collection.Note) != True:
+        raise Exception("Invalid note ID or note object")
+
+    dids = col.db.list("select distinct did from cards where nid = {0}".format(note.id))
+    #pprint(dids)
+    decks = []
+    for did in dids:
+        name = col.decks.name(did)
+        print(name)
+        decks.append({'name':name, 'id':did})
+    #pprint(decks)
+    return decks
+def getNoteInfo(note, print):
+    if isinstance(note, int):
+        note = col.get_note(note)
+
+    if isinstance(note, anki.collection.Note) != True:
+        raise Exception("Invalid note ID or note object")
+
+    fieldsAndValues = {}
+    for key, value in note._fmap.items():
+        idx = value[0]
+        name = value[1]['name']
+        value = note.fields[idx]
+        ##print('({0}) {1} : {2}'.format(idx, name, value))
+        fieldsAndValues[name] = value
+    output = {
+        'nid': note.id,
+        'fields': fieldsAndValues,
+        'model_name': getNotetypeNameFromNote(note),
+        'mid': note.mid,
+        'tags': note.tags,
+        'decks': getDecksForNote(note)
+    }
+    if print:
+        pprint(output)
+    return output
+
+output = []
+
+count = 0
+
+for noteId in noteIds:
+    output.append(getNoteInfo(noteId, True))
+    count += 1
+
+print(len(output))
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(output, f, ensure_ascii=False, indent=4)
+
+
 col.close()
